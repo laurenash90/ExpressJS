b687da5e061dea3546999e0e5b8459fac53ce5c4
laurenash90 <inmanl@mail.gvsu.edu>
2023-04-12 21:34:24 -0400

    get

--- my-server.ts
+++ my-server.ts
@@ -8,6 +8,13 @@ app.get("/", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
 
+const words = ['apple', 'banana', 'cherry', 'grape', 'lemon'];
+
+app.get('/path/goes/here', (req, res) => {
+    const response = { secret: "" };
+    res.json(response);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is listening to port ${PORT}`);
 });
